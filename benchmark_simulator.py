28ec7e6d2d6593568cd46b762610d69e77670a6f
Marcus <mark.ben9@gmail.com>
2017-07-19 14:38:37 -0700

    In progress adjusting resolution in calculation

--- benchmark_simulator.py
+++ benchmark_simulator.py
@@ -1,23 +1,32 @@
 import simulator
 import aipy
 import numpy as np
+import cProfile
+import pstats
 
 #NANTS = 350
 NANTS = 20
-NBLS = NANTS * (NANTS+1) / 2 # for HERA
+#NBLS = NANTS * (NANTS+1) / 2 # for HERA
 #NSIDE = 512
 NSIDE = 64
 #NFREQS = 1024
 NFREQS = 16
 
 h = aipy.healpix.HealpixMap(nside=NSIDE)
+
 s = np.array(h.px2crd(np.arange(h.npix()))).T
 I = np.random.normal(size=(h.npix(),NFREQS)) # sky is real-valued
 A = np.random.normal(size=(h.npix(),NFREQS)) + \
     1j*np.random.normal(size=(h.npix(),NFREQS)) # antenna response is complex
+
 fqs = np.linspace(100e6,200e6,NFREQS)
+
 antpos = np.random.normal(size=(NANTS,3))
 #bls = np.array([ai-aj for i,ai in enumerate(antpos) for aj in antpos[i:]])
 
-#vis = simulator.measurement_eq(A, I, s, bls, fqs)
-vis = simulator.measurement_eq_split(A, I, s, antpos, fqs)
+#cProfile.run('vis = simulator.measurement_eq(A, I, s, bls, fqs)','measurement_eq_split.prf' )
+cProfile.run('vis2 = simulator.measurement_eq_split(A, I, s, antpos, fqs)','measurement_eq.prf')
+
+#s1 = pstats.Stats('measurement_eq_split.prf')
+s2 = pstats.Stats('measurement_eq.prf')
+
--- simulator.py
+++ simulator.py
@@ -17,8 +17,9 @@ def measurement_eq(A, I, s, bls, fqs):
     b_s = np.dot(bls, s.T).T
     b_s.shape = (b_s.shape[0],1,b_s.shape[1]) # shape is (s,fq,bl)
     fqs.shape = (1,fqs.size,1)
-    A.shape = A.shape + (1,)
-    I.shape = I.shape + (1,)
+    if A.ndim < 3:  
+        A.shape = A.shape + (1,)
+        I.shape = I.shape + (1,)
     V = np.sum(A * I * np.exp(-2j*np.pi / C * fqs * b_s), axis=0).T
     return V # shape is (bls,fqs)
 
@@ -32,22 +33,75 @@ def measurement_eq_split(A, I, s, ant_pos, fqs):
     s_valid = s[:,2] > 0
     s_above = s[s_valid,:]
     A_above = A[s_valid,:]
-    I_above = I[s_valid,:]    
+    I_above = I[s_valid,:]
     #dot product
     a_s = np.dot(ant_pos, s_above.T).T         
     #Matrix set up
     a_s.shape = (a_s.shape[0],1,a_s.shape[1]) # shape is (s,fq,ant_pos)
     fqs.shape = (1,fqs.size,1)
-    A_above.shape = A_above.shape + (1,)
-    I_above.shape = I_above.shape + (1,)
+    if A_above.ndim < 3:  
+        A_above.shape = A_above.shape + (1,)
+        I_above.shape = I_above.shape + (1,)
     #Equation for just an antenna
     V_a = (np.sqrt(A_above) * np.sqrt(I_above) * np.exp(-2j*np.pi / C * fqs * a_s))
-    #product of two antenna-baseline
-    V = [np.sum(V_a[...,i]*V_a[...,j].conj(), axis=0 ) 
+    V_a = V_a.astype(np.complex64)
+    V_a_conj = V_a.conj()
+    print(V_a.dtype)
+    #procduct of two antenna-baseline
+    V = [np.sum(V_a[...,i]*V_a_conj[...,j], axis=0 ) 
+            for i in range(V_a.shape[-1]) for j in range(i,V_a.shape[-1])]
+    
+    return np.array(V) #shape (bls, fqs)
+   
+    
+def measurement_eq_split_dyn(A, I, s, ant_pos, fqs):
+    """
+    I feel like either this would have to be something that either the user
+    would send or, that it should be dynamical in the forloops, so if the
+    combination of ai and aj could be atoned for their own resolution. I think 
+    I should do this mathmatically, and depending on how much variation there
+    is, for a given angle of the sky, I can determine the approximate ammount 
+    of resolution. 
+    """
+#I could prossible run an if loop, inside the forloop to impliment a change how
+# an eq progresses. but then I would have to write more equations for "anttena"
+    '''A: (s,fq) 2-dim array
+    I: (s,fq) 2-dim array
+    ant_pos: (antenna's position,xyz)
+    s: (sky position,xyz)
+    fqs: (frequency) 1-dim array'''
+    #selection of points of interest
+    s_valid = s[:,2] > 0
+    s_above = s[s_valid,:]
+    #well S is given by healpix in the digonstic test
+    A_above = A[s_valid,:]
+    I_above = I[s_valid,:]
+    #dot product
+    a_s = np.dot(ant_pos, s_above.T).T         
+    #Matrix set up
+    a_s.shape = (a_s.shape[0],1,a_s.shape[1]) # shape is (s,fq,ant_pos)
+    fqs.shape = (1,fqs.size,1)
+    #safety precaution for the dimetions of the array
+    if A_above.ndim < 3:  
+        A_above.shape = A_above.shape + (1,)
+    if I_above.ndim < 3:
+        I_above.shape = I_above.shape + (1,)
+    #Equation for just an antenna
+    V_a = (np.sqrt(A_above) * np.sqrt(I_above) * np.exp(-2j*np.pi / C * fqs * a_s))
+    V_a = V_a.astype(np.complex64)
+    #Repeat V_a but for "less" sources.
+    V_a_conj = V_a.conj()
+    
+    
+    print(V_a.dtype)
+    #procduct of two antenna to give a baseline
+    #XXX need to figure a way to selective pick out antenna combinations
+    V = [np.sum(V_a[...,i]*V_a_conj[...,j], axis=0 ) 
             for i in range(V_a.shape[-1]) for j in range(i,V_a.shape[-1])]
     
     return np.array(V) #shape (bls, fqs)
     
+
 class Source:
     def __init__(self, ra, dec, jansky=100., index=-1., mfreq=150.):
         '''ra: right ascension (in radians)
